c2c409ad3d952b94a93882f9b4b4ccd31ade0b71
Marco Scheel <ich+git@marcoscheel.de>
2015-04-29 23:31:41 +0200

    Read view beta completed

--- src/SPUGFFM for OutlookWeb/AppRead/Home/Home.js
+++ src/SPUGFFM for OutlookWeb/AppRead/Home/Home.js
@@ -15,8 +15,26 @@
                 contentType: 'application/json;charset=utf-8'
             }).done(function (data) {
 
-                $('#title').text(data.Events[1].Title);
-                $('#loading').hide();
+                var latestEvent = data.Events[1];
+                $('#title').text(latestEvent.Title);
+                $('#date').text(latestEvent.Date);
+                $('#location').html(latestEvent.Location.replace(/\n/g, "<br />"));
+                $('#location').attr('href', 'http://maps.google.com/maps?q=' + latestEvent.LocationGPS);
+                $('#description').html(latestEvent.Description.replace(/\n/g, "<br />"));
+                
+                var mentions = "";
+                jQuery.each(latestEvent.Mentions, function(index, value) {
+                    mentions += "<span class=\"glyphicon glyphicon-share\"></span>  <a target=\"_blank\" href=\"http://twitter.com/" + this + "\">" + this + "</a><br/>";
+                });
+                $('#social').html(mentions);
+
+                var links = '';
+                jQuery.each(latestEvent.Links, function(index, value) {
+                    links += '<span class=\"glyphicon glyphicon-globe\"></span>  <a target=\"_blank\" href=\"' + this + '\">' + this + '</a><br/>';
+                });
+                $('#links').html(links);
+                
+                $('#loading').text('Beschreibung');
 
             }).fail(function (status) {
                 app.showNotification('Error', 'Could not communicate with the server.');
